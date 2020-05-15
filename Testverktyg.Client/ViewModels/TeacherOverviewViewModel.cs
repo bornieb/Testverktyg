2894e1d465dee8ef13ed1b4153c74befa1a05433
Jennie Fogwall <j_stenvall@hotmail.se>
2020-05-15 09:53:42 +0200

    Skelett korrigerad

--- Testverktyg.Client/ViewModels/TeacherOverviewViewModel.cs
+++ Testverktyg.Client/ViewModels/TeacherOverviewViewModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Testverktyg.Client.Views
+namespace Testverktyg.Client.ViewModels
 {
     class TeacherOverviewViewModel
     {
