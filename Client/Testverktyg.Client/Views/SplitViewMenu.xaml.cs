e2c2fc489b2bb19eae563fb983702e7e7e3274cf
Michael Ernst <Michael_Ernst@hotmail.com>
2020-05-18 11:21:32 +0200

    Lagt till SplitViewMenu branch och kod

---
+++ Client/Testverktyg.Client/Views/SplitViewMenu.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Testverktyg.Client.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SplitViewMenu : Page
+    {
+        public SplitViewMenu()
+        {
+            this.InitializeComponent();
+        }
+
+        private void CollapseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MenuSplit.IsPaneOpen)
+            {
+                MenuSplit.IsPaneOpen = false;
+                CollapseButton.Width = 56;
+            }
+            else
+            {
+                MenuSplit.IsPaneOpen = true;
+                CollapseButton.Width = 130;
+            }
+        }
+
+        private void SplitViewMenuList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
