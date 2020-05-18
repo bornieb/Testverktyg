f2dfd5cd7ce09d48b3f538d3f89bc2635c355fcb
Jennie Fogwall <j_stenvall@hotmail.se>
2020-05-18 14:36:21 +0200

    lagt till enum

--- API/TestverktygAPI/Models/Question.cs
+++ API/TestverktygAPI/Models/Question.cs
@@ -9,6 +9,7 @@ namespace TestverktygAPI.Models
     {
         public int QuestionId { get; set; }
         public int CourseId { get; set; }
+        public GradeLevel GradeLevel { get; set; }
         public string QuestionText { get; set; }
         public QuestionType QuestionType { get; set; }
         public int QuestionValue { get; set; }
