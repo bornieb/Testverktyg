e573185333152a88ce56879349a84b24db904f02
Jennie Fogwall <j_stenvall@hotmail.se>
2020-05-18 13:46:08 +0200

    added gradelevel

---
+++ Client/Testverktyg.Client/Models/GradeLevel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testverktyg.Client.Models
+{
+    public enum GradeLevel { Grade7, Grade8, Grade9 }
+}
