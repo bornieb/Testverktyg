85eb5dfce882aca89fa0968e73a7a4a28cfb460f
Michael Ernst <Michael_Ernst@hotmail.com>
2020-05-20 10:42:33 +0200

    Små ändringar, fixat så att TeacherOverView startas direkt när man loggar in

--- Client/Testverktyg.Client/Views/SplitViewMenu.xaml.cs
+++ Client/Testverktyg.Client/Views/SplitViewMenu.xaml.cs
@@ -25,6 +25,7 @@ namespace Testverktyg.Client.Views
         public SplitViewMenu()
         {
             this.InitializeComponent();
+            PageFrame.Navigate(typeof(TeacherOverview));
         }
 
         private void CollapseButton_Click(object sender, RoutedEventArgs e)
