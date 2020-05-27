e3211757a986e81b1f6d8612e6ad0fe186781d9c
Glenn Jönsson <glennj83@hotmail.com>
2020-05-27 14:29:38 +0200

    Massa ändringar

---
+++ Client/Testverktyg.Client/Services/StudentService.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using Testverktyg.Client.Models;
+using Testverktyg.Client.ViewModels;
+
+//using Windows.Web.Http;
+//using Windows.Web.Http;
+
+namespace Testverktyg.Client.Services
+{
+    class StudentService
+    {
+        private const string url = "http://localhost:60485/api/Student";
+        //private WebClient webClient = new WebClient();
+        //ObservableCollection<Question> ListOfQuestions = new ObservableCollection<Question>();
+        //HttpClient httpClient = new HttpClient();
+
+        public async Task<Student> GetUserAsync(int userId)
+        {
+            ////Serializera till string
+            //var jsonQuestion = JsonConvert.SerializeObject(userId);
+            ////Httpscontent
+            //HttpContent httpContent = new StringContent(jsonQuestion);
+            ////Format
+            //httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            ////HTTP Request
+            //HttpResponseMessage message = await httpClient.PostAsync($"{url}", httpContent);
+
+            ////Objectet i Json format
+            //var jsStudent = await message.Content.ReadAsStringAsync();
+
+            ////Deserialiserar
+            //var student = JsonConvert.DeserializeObject<Student>(jsStudent);
+
+            ////retunerar Student
+            //return student;
+
+            //Mickes
+            //var student = new List<Student>();
+            using (HttpClient httpClient = new HttpClient())
+            {
+
+                HttpResponseMessage responseMessage = await httpClient.GetAsync($"{url}/{userId}");
+                string jsStudent = await responseMessage.Content.ReadAsStringAsync();
+                var student = JsonConvert.DeserializeObject<Student>(jsStudent);
+
+                return student;
+            }
+
+        }
+
+    }
+}
--- Client/Testverktyg.Client/ViewModels/StudentOverviewViewModel.cs
+++ Client/Testverktyg.Client/ViewModels/StudentOverviewViewModel.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Testverktyg.Client.Models;
+using Testverktyg.Client.Services;
 
 
 namespace Testverktyg.Client.ViewModels
 {
-    class StudentOverviewViewModel
+    public class StudentOverviewViewModel
     {
         public Student myStudent
         {
@@ -16,10 +17,12 @@ namespace Testverktyg.Client.ViewModels
             set;
         }
 
+        StudentService studentService = new StudentService();
 
         public StudentOverviewViewModel()
         {
             myStudent = new Student(1,"Glenn", "Jönsson", "glennj83@hotmail.com", "glenn", 2);
+            Student myStudent1 = studentService.GetUserAsync(1);
         }
 
 
