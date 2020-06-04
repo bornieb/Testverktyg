ea102e98287f6d2f0ccc500d9f53fdf5b14e4a80
Glenn Jönsson <glennj83@hotmail.com>
2020-06-04 12:07:39 +0200

    Dropdown, get för teacherOverview

--- Client/Testverktyg.Client/ViewModels/TeacherOverviewViewModel.cs
+++ Client/Testverktyg.Client/ViewModels/TeacherOverviewViewModel.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Testverktyg.Client.Models;
+using Testverktyg.Client.Services;
 
 namespace Testverktyg.Client.ViewModels
 {
-    class TeacherOverviewViewModel
+    public class TeacherOverviewViewModel
     {
+        CourseService courseService = new CourseService();
+        public ObservableCollection<Course> ListCourse { get; set; } = new ObservableCollection<Course>();
+        
+
+        
+        
+        public void GetCourses()
+        {
+            var courses = courseService.GetCourses();
+            foreach (Course course in courses)
+            {
+                ListCourse.Add(course);
+            }
+        }
+
     }
 }
